5ffec417d4b5b04fae38918bc1fb59c1f0fa55f3
Nicholas Nolen <nicnolen@ymail.com>
2022-02-03 14:32:05 -0600

    added Comment model to Post and User

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -19,6 +19,16 @@ router.get('/', (req, res) => {
     ],
     order: [['created_at', 'DESC']],
     include: [
+      // Comment model
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+
       {
         model: User,
         attributes: ['username'],
@@ -51,6 +61,16 @@ router.get('/:id', (req, res) => {
       ],
     ],
     include: [
+      // Comment model
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+
       {
         model: User,
         attributes: ['username'],
