42d44e2e8b5691b4b52066e78e4c3f80dbd9b96b
Nicholas Nolen <nicnolen@ymail.com>
2022-02-07 11:48:49 -0600

    set up file struture for handlebars.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 /* COLLECT THE PACKAGED API ROUTES */
 const router = require('express').Router();
-
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+// Import dependencies
+// Import express
 const express = require('express');
-const routes = require('./routes');
+// Import sequelize
 const sequelize = require('./config/connection');
+// Import Handlebars.js
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+// Import routes
+const routes = require('./controllers/');
+
+// Import stylesheets
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set handlebars as the apps template engine of choice
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// allow the app to use stylesheet
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
