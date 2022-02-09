0a7708d598c6163a8630eba8176f27be4d45ce6f
Nicholas Nolen <nicnolen@ymail.com>
2022-02-09 12:15:32 -0600

    wrote passing tests for shortening urls

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,11 +4,21 @@ module.exports = {
       date
     ).getDate()}/${new Date(date).getFullYear()}`;
   },
+
   format_plural: (word, amount) => {
     if (amount !== 1) {
       return `${word}s`;
     }
 
     return word;
+  },
+
+  format_url: url => {
+    return url
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
   },
 };
