8737ed862c20ccfe717fe7d700a69a5afdda105a
Nicholas Nolen <nicnolen@ymail.com>
2022-02-09 11:47:14 -0600

    wrote passing tests for formating dates

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,7 @@
-const { TestWatcher } = require('jest');
+const { format_date } = require('../utils/helpers');
 
 // test to make sure that format_date() takes Date() objects and returns dates in correct format
-TestWatcher('format_date() returns a date string', () => {
+test('format_date() returns a date string', () => {
   const date = new Date('2020-03-20 16:12:03');
 
   expect(format_date(date)).toBe('3/20/2020');
