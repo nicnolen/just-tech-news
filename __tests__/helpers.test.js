c92b2d2ecbcf8ae67336d37e43a7279080179d3f
Nicholas Nolen <nicnolen@ymail.com>
2022-02-09 11:58:45 -0600

    wrote failing tests for pluralizing words

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,3 +1,5 @@
+const { format } = require('express/lib/response');
+const { pluralize } = require('sequelize/dist/lib/utils');
 const { format_date } = require('../utils/helpers');
 
 // test to make sure that format_date() takes Date() objects and returns dates in correct format
@@ -5,4 +7,13 @@ test('format_date() returns a date string', () => {
   const date = new Date('2020-03-20 16:12:03');
 
   expect(format_date(date)).toBe('3/20/2020');
+});
+
+// test to pluralize words
+test('format_plural() returns a pluralizes words', () => {
+  const word1 = format_plural('tiger', 1);
+  const word2 = format_plural('lion', 2);
+
+  expect(word1).toBe('tiger');
+  expect(word2).toBe('lions');
 });
