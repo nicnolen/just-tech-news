afa33002458674e06ba917e2f9b73321aa93d713
Nicholas Nolen <nicnolen@ymail.com>
2022-02-09 13:39:05 -0600

    added ability to add posts in app

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
